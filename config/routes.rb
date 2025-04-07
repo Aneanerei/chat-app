881eb6d86529104678e0ee0992d78b2bde1aa042
Aneanerei <tamu.m.thr@gmail.com>
2025-04-07 15:28:35 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get 'messages/index'
  root to: "messages#index"
+ resources :rooms, only: [:new, :create, :show, :index, :edit, :update, :destroy]  # 必要なアクションを追加
+ resources :messages, only: [:new, :create, :show, :index, :edit, :update, :destroy]  # 必要なアクションを追加
+
 end
